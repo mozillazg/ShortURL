c0070ac84dea114271896c33f603b9b9e0e09634
Mozillazg <opensource.mozillazg@gmail.com>
2013-01-04 17:40:18 +0800

    Modified   shorturl/db.sql

--- shorturl/db.sql
+++ shorturl/db.sql
@@ -1,13 +1,9 @@
 /* MySQL 数据库 */
-/* 创建用户 'py'，密码为 'py_passwd'，该用户对数据库 shorturl 下所有表拥有所有权限 */
-create user py identified by "py_passwd";
-grant all privileges on shorturl.* to py@'localhost' identified by "py_passwd";
-flush privileges;
+/* 创建用户 'py'，host 为 'localhost'，密码为 'py_passwd'，该用户对数据库 shorturl 下所有表拥有所有权限 */
+grant all on shorturl.* to 'py'@'localhost' identified by "py_passwd";
 
-/* 创建用户 'readonly'，密码为 'readonly_passwd'，该用户对数据库 shorturl 下所有表拥有 select 权限 */
-create user readonly identified by "readonly_passwd";
-grant select on shorturl.* to readonly@'localhost' identified by "readonly_passwd";
-flush privileges;
+/* 创建用户 'readonly'，host 为 'localhost'，密码为 'readonly_passwd'，该用户对数据库 shorturl 下所有表拥有 select 权限 */
+grant select on shorturl.* to 'readonly'@'localhost' identified by "readonly_passwd";
 
 create database shorturl default charset utf8;
 
