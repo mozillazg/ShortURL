00e4cdf6aaa3971fae17a7f945a6a77a513f69c5
Mozillazg <opensource.mozillazg@gmail.com>
2013-05-05 15:05:52 +0800

    fix template error.

--- shorturl/settings.py
+++ shorturl/settings.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+
+SITE_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 DEBUG = True  # 调试模式
-TEMPLATE_DIR = 'templates/'  # 模板目录
+TEMPLATE_DIR = os.path.join(SITE_ROOT, 'templates')  # 模板目录
 BASE_TEMPLATE = 'base'  # 基础模板
 
 # URL 映射
