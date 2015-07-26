b4f27cd8a88e5813015f753d141ccccd2bf79a51
mozillazg <opensource.mozillazg@gmail.com>
2015-07-26 22:31:21 +0800

    修复一出不兼容旧版的问题

--- shorturl/shorten/urls.py
+++ shorturl/shorten/urls.py
@@ -13,7 +13,7 @@ MIN_LENGTH = settings.SHORTEN_CODE_MIN_LENGTH
 urlpatterns = patterns(
     '',
     url(r'^$', ShortenView.as_view(), name='shorten'),
-    url(r'^s/(?P<code>\w{%s,15})/$' % MIN_LENGTH,
-        ExpandView.as_view(), name='url'),
     url(r'^qr/$', QrcodeView.as_view(), name='qrcode'),
+    url(r'^(?P<code>\w{%s,15})/$' % MIN_LENGTH,
+        ExpandView.as_view(), name='url'),
 )
